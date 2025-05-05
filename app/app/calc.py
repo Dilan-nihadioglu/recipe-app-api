e02bc80743f596c82bcc829b1da787e56cea47f1
Dilan-nihadioglu <dilannihadioglu@gmail.com>
2025-05-05 03:03:06 +0300

    Added github actions.

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,6 +5,7 @@ def add(x, y):
     """x ve y'yi toplayıp sonucu döndürür."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """x ve y'i çıkarıp sonucu döndürür. """
     return y-x
--- app/app/test.py
+++ app/app/test.py
@@ -2,7 +2,7 @@
 Örnek testler.
 """
 from django.test import SimpleTestCase
-from app import calc 
+from app import calc
 
 class CalcTests(SimpleTestCase):
     """calc modülünü test edeceğiz."""
@@ -10,9 +10,10 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """İki sayıyı toplar mı onu test ederiz."""
         res = calc.add(5, 6)
-        self.assertEqual(res, 11)  # Beklenen sonuç 11 mi? Evetse test geçer.
+        self.assertEqual(res, 11) # Beklenen sonuç 11 mi? Evetse test geçer.
+
 
     def test_sub_numbers(self):
         "İki sayıyı çıkarabiliyor onu mu test ederiz"
-        res = calc.subtract(10,15)
-        self.assertEqual(res,5)
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
