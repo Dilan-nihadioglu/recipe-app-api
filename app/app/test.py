3d144ac48195f9679e9e0e75ba6a27dafc645a26
Dilan-nihadioglu <dilannihadioglu@gmail.com>
2025-05-05 03:08:50 +0300

    Added github actions.

--- app/app/test.py
+++ app/app/test.py
@@ -4,16 +4,17 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """calc modülünü test edeceğiz."""
 
     def test_add_numbers(self):
         """İki sayıyı toplar mı onu test ederiz."""
         res = calc.add(5, 6)
-        self.assertEqual(res, 11) # Beklenen sonuç 11 mi? Evetse test geçer.
-
+        self.assertEqual(res, 11)  # Beklenen sonuç 11 mi? Evetse test geçer.
 
     def test_sub_numbers(self):
-        "İki sayıyı çıkarabiliyor onu mu test ederiz"
+        """İki sayıyı çıkarabiliyor mu onu test ederiz."""
         res = calc.subtract(10, 15)
-        self.assertEqual(res, 5)
+        self.assertEqual(res, 5)  # 15 - 10 = 5
+
